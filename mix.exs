f4684d9e21fdde685e2f2d0e70b7aa26dbb911fe
Jiří Vrba <jiri@vrba.dev>
2022-09-06 17:45:28 +0200

    Add amnesia dependency

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule ShadyUrls.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:amnesia, "~> 0.2.8"}
     ]
   end
 
