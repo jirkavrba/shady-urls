ad5a7f832e05c7eafef00964d2fc9a5bfce8951e
Jiří Vrba <jiri@vrba.dev>
2022-09-06 17:18:41 +0200

    Replace tailwind with bootstrap CDN

--- mix.exs
+++ mix.exs
@@ -43,8 +43,7 @@ defmodule ShadyUrls.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"},
-      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev}
+      {:plug_cowboy, "~> 2.5"}
     ]
   end
 
@@ -57,7 +56,7 @@ defmodule ShadyUrls.MixProject do
   defp aliases do
     [
       setup: ["deps.get"],
-      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["esbuild default --minify", "phx.digest"]
     ]
   end
 end
