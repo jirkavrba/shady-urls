2dc51cd96efa9b9b81934fb07022f192eced3d35
Jiří Vrba <jiri@vrba.dev>
2022-09-06 14:14:24 +0200

    Configure the application domain

--- config/prod.exs
+++ config/prod.exs
@@ -19,9 +19,8 @@ config :logger, level: :info
 # To get SSL working, you will need to add the `https` key
 # to the previous section and set your `:url` port to 443:
 #
-#     config :shady_urls, ShadyUrlsWeb.Endpoint,
-#       ...,
-#       url: [host: "example.com", port: 443],
+config :shady_urls, ShadyUrlsWeb.Endpoint,
+  url: [host: "0x4a69726b612.xyz", port: 443]
 #       https: [
 #         ...,
 #         port: 443,
