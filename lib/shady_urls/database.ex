0a3a3025510f84761068f0fd8eaaffd893f44de1
Jiří Vrba <jiri@vrba.dev>
2022-09-14 13:08:17 +0200

    Remove IO inspect

--- lib/shady_urls/database.ex
+++ lib/shady_urls/database.ex
@@ -7,7 +7,6 @@ defmodule ShadyUrls.Database do
 
   @spec insert_redirect(String.t(), String.t(), boolean()) :: no_return()
   def insert_redirect(path, redirect, preview) when is_binary(path) and is_binary(redirect) and is_boolean(preview) do
-    IO.inspect(preview)
     Agent.update(__MODULE__, fn db -> Map.put(db, path, {redirect, preview}) end)
   end
 
